4cd81874caa123f904abf900acf2ee26127226e4
Pedro Jose Ales Perez <pereales@gmail.com>
2023-05-27 01:06:41 +0200

    fixed show images

--- helloworlddl.py
+++ helloworlddl.py
@@ -16,12 +16,20 @@ print("labels training:", len(y_train))
 print("images test:", x_test.shape[0])
 print("labels test:", len(y_test))
 
-# show example image loaded number [n]
-plt.ion() 
-plt.imshow(x_train[9], cmap=plt.cm.binary)
-plt.show(block = False)
-#show example label from image loaded number [n]
-print("label:", y_train[9])
+# show example 25 images loaded and labels
+def plot_images(images, labels):
+    plt.figure(figsize=(10, 10))
+    for i in range(25):
+        plt.subplot(5, 5, i+1)
+        plt.imshow(images[i], cmap=plt.cm.binary)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.xlabel(labels[i])
+    plt.show()
+    plt.show(block = False)
+# show images(x) and labels(y) from dataset
+plot_images(x_train[:25], y_train[:25])
 
 # Set the print options for a NumPy array 
 np.set_printoptions(precision=2, suppress=True, linewidth=120)
@@ -50,30 +58,68 @@ model.summary()
 # and accuracy as the evaluation metric
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy']) 
 
-# Train the model on training data for two epochs
-model.fit(x_train, y_train, epochs=2)
+# Train the model and get the training history
+history = model.fit(x_train, y_train, epochs=2, validation_data=(x_test, y_test))
+
+# Get loss curves and history accuracy
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+accuracy = history.history['accuracy']
+val_accuracy = history.history['val_accuracy']
+
+# Plot loss curves
+plt.plot(loss, label='Train Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+# Plot accuracy curves
+plt.plot(accuracy, label='Train Accuracy')
+plt.plot(val_accuracy, label='Validation Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+plt.show(block = False)
+
 
 # Evaluate the model on test data and print (verbose=2) the test loss and accuracy
 model.evaluate(x_test,  y_test, verbose=2)
 
 # Get the weights of the model
 w = model.get_weights()
-print ("layers:",len(w))
+print("Number of layers:", len(w))
 if len(w) > 0:
-  # Convert the weights of the third layer to a numpy array
-  w = np.asarray(w[0])
-  # Visualize the first column of weights as an image
-  plt.figure()
-  # show number 4
-  # blue color is a good zone
-  plt.imshow(w[:, 4].reshape([28, 28]), cmap=plt.get_cmap('seismic_r'))
-  plt.show(block=False)
+    # Convert the weights of the first layer to a numpy array
+    w_ = np.asarray(w[0])
+    # Visualize the weights of each neuron as an image
+    plt.figure(figsize=(10, 4))
+    num_neurons = w_.shape[1]
+    for i in range(num_neurons):
+        plt.subplot(2, 10, i+1)
+        plt.imshow(w_[:, i].reshape((28, 28)), cmap=plt.get_cmap('seismic_r'))
+        plt.xticks([])
+        plt.yticks([])
+        plt.title("Neuron {}".format(i + 1)) 
+        plt.xlabel("W[{}, {}]".format(i, 0)) 
+    plt.tight_layout() 
+    plt.show()
+    # The blue color represents a good zone
 else:
-  print("insufficient layers")
+    print("Insufficient layers")
+
+
+
+
+
+
+
 
 #print array corresponding to image weights of number 4
 np.set_printoptions(precision=0, suppress = True, linewidth=120)
-print(np.matrix (255*(w[:,4].reshape([28,28]))))
+print(np.matrix (255*(w_[:,4].reshape([28,28]))))
 
 
 
